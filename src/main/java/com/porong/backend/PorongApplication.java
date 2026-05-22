94b4a686d1119eeac7f92ff4dcb4d67406fb9008
JuhyunLee <hanaheart030@gmail.com>
2026-05-22 13:59:58 +0900

    Chore: 스프링 부트 초기 패키지 구조 세팅

---
+++ src/main/java/com/porong/backend/PorongApplication.java
@@ -0,0 +1,13 @@
+package com.porong.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorongApplication.class, args);
+	}
+
+}
