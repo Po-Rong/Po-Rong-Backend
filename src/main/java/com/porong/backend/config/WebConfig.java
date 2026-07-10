df3d121d5c903a719221ab96e363dab2b74512f5
JuhyunLee <hanaheart030@gmail.com>
2026-07-10 15:23:25 +0900

    feat: 프론트엔드 주소 추가 방어 코딩

--- src/main/java/com/porong/backend/config/WebConfig.java
+++ src/main/java/com/porong/backend/config/WebConfig.java
@@ -21,8 +21,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+                .allowedOrigins(
+                "http://127.0.0.1:5500", 
+                "http://localhost:5500",
+                "https://your-project-name.vercel.app"
+            )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
