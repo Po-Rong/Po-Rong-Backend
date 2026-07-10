592fc427c33612f30b98b29bc2cc2c1f2c1ef1c9
JuhyunLee <hanaheart030@gmail.com>
2026-07-10 15:44:01 +0900

    fix: 통신 오류 수정

--- src/main/java/com/porong/backend/config/WebConfig.java
+++ src/main/java/com/porong/backend/config/WebConfig.java
@@ -21,6 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins(
+                    "http://127.0.0.1:5500", 
+                    "http://localhost:5500",
+                    "https://po-rong-frontend.vercel.app"
+                )
+
                 .allowedOriginPatterns(
                     "http://127.0.0.1:5500", 
                     "http://localhost:5500",
